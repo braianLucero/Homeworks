a2676e13cdfa3e7c4504cc036065f5e6bb0ee375
brian Lucero <brasanluc123@gmail.com>
2022-11-28 13:09:10 -0300

    Stilos

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -3,14 +3,21 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
   if (typeof startEl === "undefined") {
     startEl = document.body;
-    sad
+    
   }
 
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if(matchFunc(startEl)) resultSet.push(startEl);
   
+  for (let i = 0;  startEl.children.length; i++) {
+    let child = startEl.children[i];
+   let elementsFound =  traverseDomAndCollectElements(matchFunc,child);
+    resultSet = [...resultSet, ...elementsFound];
+  }
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -19,7 +26,13 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === "#"){return "id"};
+
+  if(selector[0] === "."){return "class"};
+
+  if (selector.includes(".")  ){return "tag.class"};
+
+  return "tag";
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -31,15 +44,34 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = function(element){
+     return '#' + element.id === selector ;
+    }
+
   } else if (selectorType === "class") {
-    
+    matchFunction = function(element){
+      let classes = element.classList;
+      for (let i = 0; i < classes.length; i++) {
+        if ('.'+ classes[i] === selector){
+          return true;
+        }
+      }
+      return false;
+    }
+
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = function(element){
+      let [t,c] = selector.split('.')
+      return matchFunctionMaker(t)(element) && matchFunctionMaker ('.' + c ) (element);
+    }
+ 
   } else if (selectorType === "tag") {
-    
+    matchFunction = function(element){
+      return element.tagName === selector.toUpperCase();
+    }
   }
   return matchFunction;
+
 };
 
 var $ = function(selector) {
